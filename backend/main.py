680c0518ba9d60adca0c70ad40440c4ec791fcf6
Amila619 <amilasandeepa349@gmail.com>
2026-01-10 15:20:20 +0530

    - the API calculates risk successfully based on digital_references,  word_count and  based on novelty

--- backend/main.py
+++ backend/main.py
@@ -18,16 +18,28 @@ def root():
 def get_items(db=Depends(get_db)):
     if db is not None:
         items_ref = db.collection("cultural_items")
-        items = [doc.to_dict() for doc in items_ref.stream()]
-        return {"items": items}
+        items = []
+        for doc in items_ref.stream():
+            doc = doc.to_dict()
+            del doc["embedding"]
+            items.append(doc)
+        return items
     return {"message": "Issue connecting to the database"}
-
 
 @app.post("/items")
-def add_item(data: dict, user=Depends(verify_firebase_token), db=Depends(get_db)):
+def add_item(data: dict, db=Depends(get_db)):
     if db is not None:
-        uid = user["uid"]
+        data["user_id"] = "9RgLzrYBmFYzlIfgHeWjTGb84442"
         doc_ref = db.collection("cultural_items").document()
         doc_ref.set(data)
-        return {"message" : "entry successfully added"}
+        return {"message" : f"Entry successfully added {doc_ref.id}"}
     return {"message": "Issue connecting to the database"}
+
+# @app.post("/items")
+# def add_item(data: dict, user=Depends(verify_firebase_token), db=Depends(get_db)):
+#     if db is not None:
+#         data["user_id"] = user["uid"]
+#         doc_ref = db.collection("cl_vectors_test").document()
+#         doc_ref.set(data)
+#         return {"message" : "entry successfully added"}
+#     return {"message": "Issue connecting to the database"}
